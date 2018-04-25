b06726dab5c6a0b3d8db67a13599bf0f085f3bca
Jaden Young <jaden.young44@gmail.com>
2018-04-24 23:42:32 -0500

    Initial working version (no tests)

---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"log"
+	"os"
+	"runtime"
+
+	"github.com/cenkalti/backoff"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "rabbit-uri",
+			Value:  "amqp://guest:guest@localhost:5672/",
+			EnvVar: "RABBIT_URI",
+			Usage:  "RabbitMQ connection string",
+		},
+		cli.StringFlag{
+			Name:   "rabbit-queue",
+			Value:  "vici",
+			EnvVar: "RABBIT_QUEUE",
+			Usage:  "Name of RabbitMQ queue to declare and consume from",
+		},
+		cli.StringFlag{
+			Name:   "rabbit-exchange",
+			Value:  "eiffel",
+			EnvVar: "RABBIT_EXCHANGE",
+			Usage:  "Name of the exchange to build [QUEUE] to",
+		},
+		cli.StringFlag{
+			Name:   "rabbit-exchange-type",
+			Value:  "topic",
+			EnvVar: "RABBIT_EXCHANGE_TYPE",
+			Usage:  "Type of [EXCHANGE]",
+		},
+		cli.StringFlag{
+			Name:   "rabbit-routing-key",
+			Value:  "eiffel.#",
+			EnvVar: "RABBIT_ROUTING_KEY",
+			Usage:  "Routing key to use to bind [QUEUE] to [EXCHANGE]",
+		},
+		cli.StringFlag{
+			Name:   "rabbit-consumer-tag",
+			Value:  "plumb-rabbit-to-mongo",
+			EnvVar: "RABBIT_CONSUMER_TAG",
+			Usage:  "String to identify this consumer with the RabbitMQ broker",
+		},
+		cli.StringFlag{
+			Name:   "mongo-uri",
+			Value:  "mongodb://localhost/dbname",
+			EnvVar: "MONGO_URI",
+			Usage:  "MongoDB connection string. MUST contian database",
+		},
+		cli.StringFlag{
+			Name:   "mongo-collection",
+			Value:  "",
+			EnvVar: "MONGO_COLLECTION",
+			Usage:  "Name of the mongo collection to save to",
+		},
+	}
+	app.Action = func(c *cli.Context) error {
+		consumer := NewConsumer(
+			c.String("rabbit-consumer-tag"),
+			c.String("rabbit-uri"),
+			c.String("rabbit-exchange"),
+			c.String("rabbit-exchange-type"),
+			c.String("rabbitBindingKey"))
+
+		log.Print("Connecting to RabbitMQ")
+		err := backoff.Retry(consumer.Connect, backoff.NewExponentialBackOff())
+		if err != nil {
+			panic(err)
+		}
+
+		log.Print("Announcing queue")
+		deliveries, err := consumer.AnnounceQueue(
+			c.String("rabbit-queue"),
+			c.String("rabbitBindingKey"))
+		if err != nil {
+			panic(err)
+		}
+
+		log.Print("Connecting to Mongo")
+		saver := NewSaver(c.String("mongo-uri"), c.String("mongo-collection"))
+		err = backoff.Retry(saver.Connect, backoff.NewExponentialBackOff())
+		if err != nil {
+			panic(err)
+		}
+		defer saver.Close()
+
+		consumer.Handle(
+			deliveries,
+			saver.SaveAllDeliveries,
+			runtime.GOMAXPROCS(0),
+			c.String("rabbit-queue"),
+			c.String("rabbit-routing-key"))
+
+		log.Print("Finished handling.")
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Print("Goodbye!")
+}
---
+++ saver.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/globalsign/mgo"
+	"github.com/streadway/amqp"
+)
+
+// Saver represents a connection to a single Mongo collection
+// and allows single-threaded saving to that collection
+type Saver struct {
+	uri        string
+	session    *mgo.Session
+	collection string
+}
+
+// NewSaver instantiates a new Saver. Still need to call Connect
+// before performing any operations.
+func NewSaver(uri, collection string) *Saver {
+	return &Saver{
+		uri:        uri,
+		collection: collection,
+	}
+}
+
+// Connect establishes a connection to the underlying MongoDB of the saver
+func (s *Saver) Connect() error {
+	sess, err := mgo.Dial(s.uri)
+	if err != nil {
+		return err
+	}
+	sess.SetMode(mgo.Monotonic, true)
+	s.session = sess
+	return nil
+}
+
+// Close cleans up resources used by the Saver
+func (s *Saver) Close() {
+	if s.session != nil {
+		s.session.Close()
+	}
+	s.session = nil
+}
+
+// SaveAllDeliveries persists the body of any recieved delivery if it is
+// valid json/bson. Assumes that the consuming channel is not set to
+// auto ack, as this method DOES ack on successful save, and nack on errors.
+func (s *Saver) SaveAllDeliveries(deliveries <-chan amqp.Delivery) {
+	sess := s.session.Copy()
+	defer sess.Close()
+
+	// We assume DB was set in the uri
+	c := sess.DB("").C(s.collection)
+
+	for d := range deliveries {
+		var val map[string]interface{}
+		err := json.Unmarshal(d.Body, &val)
+		if err != nil {
+			log.Printf("Couldn't parse message as JSON: %s", err.Error())
+			d.Nack(false, true)
+		} else {
+			err := c.Insert(val)
+			if err != nil {
+				log.Printf("Error saving message: %v", err)
+				d.Nack(false, true)
+			} else {
+				d.Ack(false)
+			}
+		}
+	}
+	return
+}
